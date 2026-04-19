7f687fd90539a2ac504891c789c1458fdc0a4027
Mirjam <mirjam.mclachlan@gmail.com>
2026-04-19 19:53:10 +0200

    refactor: extract cat data to CatRepository (domain)

--- app/src/main/java/com/example/purrsistence/ui/screens/ShopScreen.kt
+++ app/src/main/java/com/example/purrsistence/ui/screens/ShopScreen.kt
@@ -7,11 +7,10 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.purrsistence.R
+import com.example.purrsistence.domain.cat.CatRepository
 import com.example.purrsistence.ui.components.CurrencyBadge
 import com.example.purrsistence.ui.components.ShopItemCard
 import com.example.purrsistence.ui.components.TopBar
-import com.example.purrsistence.ui.state.ShopItem
 import com.example.purrsistence.ui.viewmodel.UserViewModel
 
 @Composable
@@ -23,16 +22,8 @@ fun ShopScreen(
     val balance = user?.balance ?: 0
     val collectedCats = user?.collectedCatsIds ?: emptyList()
 
-    val shopItems = remember { listOf(
-        ShopItem("cat_1", "Orange Cat", 2, R.drawable.cat_orange),
-        ShopItem("cat_2", "Black Cat", 13, R.drawable.cat_black),
-        ShopItem("cat_3", "White Cat", 20, R.drawable.cat_white),
-        ShopItem("cat_4", "Brown Cat", 100, R.drawable.cat_brown),
-        ShopItem("cat_5", "Gray Cat", 150, R.drawable.cat_gray),
-        ShopItem("cat_6", "Spotted Cat", 200, R.drawable.cat_spotted),
-        ShopItem("cat_7", "Siamese Cat", 300, R.drawable.cat_siamese),
-        ShopItem("cat_8", "Lucky Cat", 500, R.drawable.cat_lucky)
-    ) }
+    // Get list of all cats (+ details) available
+    val shopItems = CatRepository.cats
 
     Column(
         modifier = Modifier
